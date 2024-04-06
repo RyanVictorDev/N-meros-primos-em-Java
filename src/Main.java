b19e7e221204577b7c585274aae06bab74d98c6e
RyanVictorDev <vryan8294@gmail.com>
2024-04-06 15:55:54 -0300

    primeiro commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+import java.util.Scanner;
+public class Main{
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        //variavel que será usada para contar quantas divisões acontecerão.
+        int contadorDivisao = 0;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Digite um número inteiro: ");
+        int numero = sc.nextInt();
+
+        //Estrutura de repetição for utilizada para dividir o número digitado de 1 até ele mesmo.
+        for (int i = 1; i <= numero; i++){
+            //Estrutura de condição if para checkar se o resto da divisão é 0.
+            if (numero%i == 0){
+                //se o resto for 0, a variável contadorDivisão receberá +1.
+                contadorDivisao++;
+            }
+        }
+        //Estrutura de condição que verifica a quantidade de divisões feita.
+        if (contadorDivisao == 2){
+            //se a quantidade for 2, o número será primo.
+            System.out.println("O número é primo!");
+        } else {
+            //caso não, não será primo.
+            System.out.println("O número não é primo.");
         }
     }
 }
